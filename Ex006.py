e4ec9e8175fa3ba21397433a4641491c8ae43da4
gustavoaquiito <gustaviprimeiro@gmail.com>
2021-05-25 16:14:46 -0400

    Ex007

---
+++ Ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um número: '))
+s1 = n * 2
+s2 = n * 3
+s3 = n ** (1/2)
+print(f'O dobro de {n} vale {s1}')
+print(f'O triplo de {n} vale {s2}')
+print(f'A raiz quadrada de {n} é igual a {s3:.2f}')
